3020635e0662de3c2311bca13fd279082a42be2b
D4sh12 <mayarkulthum@gmail.com>
2022-12-31 01:40:03 +0200

    Create client model

---
+++ models/client.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { userSchema } = require("./user.model");
+
+const status = Object.freeze(["ACTIVE", "INACTIVE"]);
+
+
+const Client = new Schema({
+    accountantId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "user",
+    },
+    legalNumber: { type: String, required: [true, "please provide your legal number"] },
+    homeNumber: { type: String, required: [true, "please provide your home number"] },
+    city: { type: String, required: [true, "please provide your city"] },
+    zipCode: { type: String, required: [true, "please provide your zip code"] },
+    jobType: { type: String, required: [true, "please provide your type of job"] },
+    companyType: { type: String, required: [true, "please provide your type of Company"] },
+    bankName: { type: String, required: [true, "please provide your bank name"] },
+    iban: { type: String, required: [true, "please provide your IBAN"] },
+    mobileNumber: { type: String, required: [true, "please provide your mobile number"] },
+    taxId: { type: String, default: "" },
+    notes: { type: String, default: "" },
+    status: {
+        type: String,
+        trim: true,
+        enum: status,
+        required: true,
+        default: "ACTIVE",
+    },
+});
+
+
+const ClientModel = userSchema.discriminator("client", Client);
+
+Client.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject.password;
+
+    },
+});
+
+module.exports = { ClientModel };
