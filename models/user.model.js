e753983959db3856b4932b46245bf2ad9affcae6
D4sh12 <mayarkulthum@gmail.com>
2022-12-31 00:12:18 +0200

    Add schema validator to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 ///? This model contains the base user model
+const uniqueValidator = require('mongoose-unique-validator');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -11,7 +12,7 @@ const baseOption = {
 
 const userSchema = new Schema(
     {
-        email: { type: String, required: [true, "please provide your email"] },
+        email: { type: String, unique: true, required: [true, "please provide your email"] },
         firstName: { type: String, trim: true, required: [true, "please provide your first name"] },
         lastName: { type: String, trim: true, required: [true, "please provide your last name"] },
         password: { type: String, required: [true, "please provide your password"] },
@@ -19,6 +20,8 @@ const userSchema = new Schema(
     },
     baseOption
 );
+
+userSchema.plugin(uniqueValidator, { message: "Email already used" });
 
 module.exports.userSchema = mongoose.model(
     "userSchema", userSchema
