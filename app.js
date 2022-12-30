54b14f79313d5a26637c8a0d5e7e6dd6078c7e4c
D4sh12 <mayarkulthum@gmail.com>
2022-12-31 01:06:59 +0200

    Create routes for admin and user

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const errors = require('./middlewares/errors');
+const uploader = require('./middlewares/upload');
 const bodyParser = require('body-parser');
 require("dotenv").config();
 
@@ -14,5 +16,14 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(uploader.single('image')); 
+
+//? Initial the routes
+app.use("/api/user", require("./routes/user.routes"));
+app.use("/api/admin", require("./routes/admin.routes"));
+// Statics files
+app.use('/uploads', express.static('uploads/'));
+
+app.use(errors.errorHandler);
 
 module.exports = { app };
